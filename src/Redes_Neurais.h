cf46096beabe18924da194c976e670fa5820a634
Bruno-Felix <bruno-a.f@hotmail.com>
2018-11-29 15:21:29 -0200

    Corrigindo bugs Leitura dos Vetores

--- src/Redes_Neurais.h
+++ src/Redes_Neurais.h
@@ -3,8 +3,12 @@
 
 double **leituraVetorGrama();
 double **leituraVetorAsfalto();
+
 int *criarVetorStatusTreinamento();
+int *criarVetorStatusTeste();
+
 double **criarMatrizTreinamento(int *VetorStatusTreinamento);
+double **criarMatrizTeste(int *criarVetorStatusTreinamento);
 
 
 #endif
