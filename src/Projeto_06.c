f58d1601a2fde5efb58f183d2e32755ce1419f2b
Bruno-Felix <bruno-a.f@hotmail.com>
2018-11-29 14:24:02 -0200

    Inserindo comentários

--- src/Projeto_06.c
+++ src/Projeto_06.c
@@ -8,8 +8,9 @@
 
 //MAIN_____________________________________//
 int main(){
-    
+    //vetorStatusTreinamento: 0 -> Asfalto | 1 -> Grama
     int *vetorStatusTreinamento = criarVetorStatusTreinamento();
+    //matrizTreinamento: vetor[536] de cada imagem de treinamento
     double **matrizTreinamento = criarMatrizTreinamento(vetorStatusTreinamento);
     
     
